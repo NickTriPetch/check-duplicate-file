26846de65cb12e79e8ac16fb0218d552f06a9281
Chaiwat Tungtongsoontorn <chaiwat_t@tripetch-it.co.th>
2022-07-10 17:43:05 +0700

    feat: check duplicated file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import CryptoJS from 'crypto-js'
 
 function App() {
   const file1 = useRef(null);
   const file2 = useRef(null);
 
-  const onFileValueChange = (fileName, fileRef) => () => {
+  const [hashFile1, setHashFile1] = useState(null);
+  const [hashFile2, setHashFile2] = useState(null);
+
+  const strOutPut = (() => {
+    if (!hashFile1 && !hashFile2) {
+      return "";
+    } else if (!hashFile1 && hashFile2) {
+      return "Please insert File1.";
+    } else if (hashFile1 && !hashFile2) {
+      return "Please insert File1.";
+    } else if (hashFile1 === hashFile2) {
+      return "File1 is as same as File2.";
+    }
+
+    return "File1 is as 'not' same as File2.";
+  })();
+
+  const onFileValueChange = (fileRef, callbackFn) => () => {
     const [file] = fileRef.current.files;
 
     if (!file) {
@@ -21,22 +39,28 @@ function App() {
   
       reader.addEventListener('load', readFileContent);
       reader.readAsText(file);
-    }).then(textBuffer => console.log(`textBuffer[${fileName}] - `, textBuffer))
+    }).then(textBuffer => {
+      return CryptoJS.SHA256(textBuffer, { outputLength: 12 }).toString(CryptoJS.enc.Hex);
+    })
+      .then(callbackFn)
       .finally(() => reader = null);
   };
 
   return (
     <div className="App">
+      {strOutPut && <h1>
+        {strOutPut}
+      </h1>}
       <section>
-        <h2>File 1</h2>
+        <h2>File 1: {hashFile1}</h2>
         <div>
-          <input type="file" ref={file1} onChange={onFileValueChange('file1', file1)} />
+          <input type="file" ref={file1} onChange={onFileValueChange(file1, setHashFile1)} />
         </div>
       </section>
       <section>
-        <h2>File 2</h2>
+        <h2>File 2: {hashFile2}</h2>
         <div>
-          <input type="file" ref={file2} onChange={onFileValueChange('file2', file2)} />
+          <input type="file" ref={file2} onChange={onFileValueChange(file2, setHashFile2)} />
         </div>
       </section>
     </div>
